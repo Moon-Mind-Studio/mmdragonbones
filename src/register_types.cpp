8493df4f5619905c01f1e07d8aa6e075bb82c5b8
MarxEckertBrasil <marxeckertbrasil@gmail.com>
2023-02-25 14:35:55 -0300

    fix: add gdextension

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -15,3 +15,16 @@ void initialize_mmdragonbones_module(ModuleInitializationLevel p_level) {
 void uninitialize_mmdragonbones_module(ModuleInitializationLevel p_level) {
 
 }
+
+extern "C" {
+// Initialization.
+GDExtensionBool GDE_EXPORT mmdragonbones_library_init(const GDExtensionInterface *p_interface, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+	godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
+
+	init_obj.register_initializer(initialize_mmdragonbones_module);
+	init_obj.register_terminator(uninitialize_mmdragonbones_module);
+	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+
+	return init_obj.init();
+}
+}
